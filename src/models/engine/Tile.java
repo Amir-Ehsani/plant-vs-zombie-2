5eb5d190bff1767b99e3b01930155a16aa5ea35c
Mohammad Khosravi <m.khosravii1313@gmail.com>
2026-06-13 15:15:57 +0330

    added skeleton for Tile

---
+++ src/models/engine/Tile.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.util.List;
+
+public class Tile {
+    private Position position;
+    private Plant currentPlant;
+    private List<Zombie> zombies;
+
+    public boolean hasPlant() {
+        return currentPlant != null;
+    }
+
+    public void addZombie(Zombie z) {
+    }
+}
