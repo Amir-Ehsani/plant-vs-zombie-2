cdeb7444d8dc09ac858dd4cc1ae1184ed42eda8b
Mohammad Khosravi <m.khosravii1313@gmail.com>
2026-06-13 15:26:00 +0330

    added skeleton for LawnMower

---
+++ src/models/entities/LawnMower.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class LawnMower {
+    private int assignedRow;
+    private boolean isTriggered;
+    private Position position; // Composition
+
+    public void destroyZombies() { }
+}
