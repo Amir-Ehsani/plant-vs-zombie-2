968f11dbd8ef9831f3dbde2113d5e42c0db4d93d
amir ehsani <a.ehsani.nik@gmail.com>
2026-06-13 20:25:11 +0000

    added skeleton for PlantUpgrade

---
+++ src/models/core/plant/PlantUpgrade.java
@@ -0,0 +1,10 @@
+package plant;
+
+public class PlantUpgrade {
+    private int requiredSeedPackets;
+    private int coinCost;
+    private int nextLevel;
+
+    public void applyUpgrade(Plant plant) {
+    }
+}
