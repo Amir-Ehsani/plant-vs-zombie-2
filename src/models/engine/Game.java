56008c4a2a46049ce11bce97be25c86de7ddb7bc
Mohammad Khosravi <m.khosravii1313@gmail.com>
2026-06-13 15:08:23 +0330

    added skeleton for Game

---
+++ src/models/engine/Game.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Game {
+    private GameSession currentSession;
+
+    public void setCurrentSession(GameSession currentSession) {
+        this.currentSession = currentSession;
+    }
+
+    public void startGame() {
+    }
+
+    public void saveProgress() {
+    }
+}
