27f75bac8ea6f41c4e7be7a3b2b1a92c3eadcc4b
amir ehsani <a.ehsani.nik@gmail.com>
2026-06-13 20:29:55 +0000

    added skeleton for ZombieType

---
+++ src/models/core/zombie/ZombieType.java
@@ -0,0 +1,29 @@
+package zombie;
+
+public class ZombieType {
+    private String name;
+    private int baseHp;
+    private double speed;
+    private int damagePerTick;
+    private int waveCost;
+
+    public String getName() {
+        return null;
+    }
+
+    public int getBaseHp() {
+        return 0;
+    }
+
+    public double getSpeed() {
+        return 0;
+    }
+
+    public int getDamagePerTick() {
+        return 0;
+    }
+
+    public int getWaveCost() {
+        return 0;
+    }
+}
