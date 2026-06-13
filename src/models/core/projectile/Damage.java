95fa7e591d2ff993b60028c3928ccab7f6299c1d
amir ehsani <a.ehsani.nik@gmail.com>
2026-06-13 20:32:51 +0000

    added skeleton for Damage

---
+++ src/models/core/projectile/Damage.java
@@ -0,0 +1,14 @@
+package projectile;
+
+public class Damage {
+    private int amount;
+    private String type;
+
+    public int getAmount() {
+        return 0;
+    }
+
+    public String getType() {
+        return null;
+    }
+}
