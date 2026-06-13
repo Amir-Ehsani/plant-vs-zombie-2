4fa469359fb17b556c80bec62ee01478b71acf7f
Mohammad Khosravi <m.khosravii1313@gmail.com>
2026-06-13 15:22:56 +0330

    added skeleton for WaveManager

---
+++ src/models/level/WaveManager.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.util.List;
+
+public class WaveManager {
+    private List<Wave> waves;
+    private int currentWaveIndex;
+
+    public void updateTicks() {
+    }
+
+    public void spawnNextWave() {
+    }
+}
