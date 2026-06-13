6a07b1d42f8570b296228899e2e22bc8b8348c70
Mohammad Khosravi <m.khosravii1313@gmail.com>
2026-06-13 14:44:35 +0330

    Added skeleton for GameController

---
+++ src/controllers/core/GameController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import Model.GameSession;
+import Model.Position;
+
+public class GameController {
+    private GameSession gameSession;
+
+
+    public void setGameSession(GameSession gameSession) {
+        this.gameSession = gameSession;
+    }
+
+    public void handleUserClick(Position pos) {
+    }
+
+    public void handlePause() {
+    }
+
+    public void update() {
+    }
+}
