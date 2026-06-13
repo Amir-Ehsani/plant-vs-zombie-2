4238ae23fb7794734b5a8b011a708e87ec4acf04
amir ehsani <a.ehsani.nik@gmail.com>
2026-06-13 20:20:12 +0000

    added skeleton for GameEntity

---
+++ src/models/core/base/GameEntity.java
@@ -0,0 +1,14 @@
+package base;
+
+public abstract class GameEntity {
+    protected String id;
+    protected int hp;
+    protected int maxHp;
+    protected double x;
+    protected double y;
+
+    public abstract void takeDamage(Damage damage);
+    public abstract boolean isAlive();
+    public abstract double getX();
+    public abstract double getY();
+}
