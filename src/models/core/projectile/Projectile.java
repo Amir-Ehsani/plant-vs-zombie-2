804231e1248e1336a3072a652f78d4e0cf15deb0
amir ehsani <a.ehsani.nik@gmail.com>
2026-06-13 20:32:14 +0000

    added skeleton for Projectile

---
+++ src/models/core/projectile/Projectile.java
@@ -0,0 +1,30 @@
+package projectile;
+
+import base.GameEntity;
+
+public class Projectile extends GameEntity {
+    private double speed;
+    private double directionX;
+    private double directionY;
+
+    public void move() {
+    }
+
+    public void onCollision(GameEntity entity) {
+    }
+
+    public void takeDamage(Damage damage) {
+    }
+
+    public boolean isAlive() {
+        return false;
+    }
+
+    public double getX() {
+        return 0;
+    }
+
+    public double getY() {
+        return 0;
+    }
+}
