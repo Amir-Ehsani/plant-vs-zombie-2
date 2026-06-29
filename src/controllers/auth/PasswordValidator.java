b3223d5ebbee9e9549a7bc926b0e6f081d3eb3da
Mohammad Khosravi <m.khosravii1313@gmail.com>
2026-06-29 19:39:12 +0330

    completed PasswordValidator and isValid method now returns Result instead of boolean

--- src/controllers/auth/PasswordValidator.java
+++ src/controllers/auth/PasswordValidator.java
@@ -1,6 +1,41 @@
 package controllers.auth;
 
+import models.Result;
+
 public class PasswordValidator {
-    public static boolean isValid(String password) {return false;}
+    private static final String ALLOWED_PASSWORD_REGEX =
+            "^[a-zA-Z0-9!#$%^&*()=+{}\\[\\]|/\\\\:;'\",><?]+$";
+
+    public static Result isValid(String password) {
+        if (password.isEmpty()) {
+            return new Result(false, "Password cannot be empty.");
+        }
+
+        if (!password.matches(ALLOWED_PASSWORD_REGEX)) {
+            return new Result(false, "Password contains invalid characters.");
+        }
+
+        if (password.length() < 8) {
+            return new Result(false, "Password is weak: it must be at least 8 characters long.");
+        }
+
+        if (!password.matches(".*[a-z].*")) {
+            return new Result(false, "Password is weak: it must contain at least one lowercase letter.");
+        }
+
+        if (!password.matches(".*[A-Z].*")) {
+            return new Result(false, "Password is weak: it must contain at least one uppercase letter.");
+        }
+
+        if (!password.matches(".*[0-9].*")) {
+            return new Result(false, "Password is weak: it must contain at least one digit.");
+        }
+
+        if (!password.matches(".*[!#$%^&*()=+{}\\[\\]|/\\\\:;'\",><?].*")) {
+            return new Result(false, "Password is weak: it must contain at least one special character.");
+        }
+
+        return new Result(true, "");
+    }
 
 }
