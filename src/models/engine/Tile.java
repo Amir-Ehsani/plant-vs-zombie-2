a39241def8255bc5d4e0a402e2529f328fcf0182
Mohsen99 <mrfeyzipoormf1001@gmail.com>
2026-06-30 13:54:19 +0330

    modified Tile.java by adding two constructors, one for normal tiles and the other one for special Tiles

--- src/models/engine/Tile.java
+++ src/models/engine/Tile.java
@@ -5,12 +5,32 @@ package models.engine;
 import models.core.plant.Plant;
 import models.core.zombie.Zombie;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Tile {
-    private Position position;
+    private final Position position;
+    private final List<Zombie> zombies;
     private Plant currentPlant;
-    private List<Zombie> zombies;
+    private TileType tileType;
+
+    public Tile(Position position) {
+        this(position, TileType.NORMAL);
+    }
+
+    public Tile(Position position, TileType tileType ) {
+        if (position == null) {
+            throw new IllegalArgumentException("Tile position cannot be null.");
+        }
+        if (tileType == null) {
+            throw new IllegalArgumentException("Tile type cannot be null.");
+        }
+
+
+        this.position = position;
+        this.tileType = tileType;
+        this.zombies = new ArrayList<>();
+    }
 
     public boolean hasPlant() {
         return currentPlant != null;
