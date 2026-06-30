09099fc0b50d83fc001197f120f026cdef0d860a
Mohsen99 <mrfeyzipoormf1001@gmail.com>
2026-06-30 14:58:10 +0330

    added & implement getters methodes in Tile.java

--- src/models/engine/Tile.java
+++ src/models/engine/Tile.java
@@ -6,6 +6,7 @@ import models.core.plant.Plant;
 import models.core.zombie.Zombie;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Tile {
@@ -26,15 +27,42 @@ public class Tile {
             throw new IllegalArgumentException("Tile type cannot be null.");
         }
 
-
         this.position = position;
         this.tileType = tileType;
         this.zombies = new ArrayList<>();
     }
 
+    //getters
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public TileType getTileType() {
+        return tileType;
+    }
+
+    public Plant getCurrentPlant() {
+        return currentPlant;
+    }
+
     public boolean hasPlant() {
         return currentPlant != null;
     }
+
+    public List<Zombie> getZombies() {
+        return Collections.unmodifiableList(zombies);
+    }
+
+    public boolean hasZombies() {
+        return !zombies.isEmpty();
+    }
+
+    public boolean isPlantable() {
+        return tileType == TileType.NORMAL;
+    }
+
+
 
     public void addZombie(Zombie z) {
     }
