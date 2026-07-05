de10d48ca0f08cb091c4ba8dc88d5e015c48a2cc
amir ehsani <a.ehsani.nik@gmail.com>
2026-07-05 15:58:01 +0000

    implemented ZombieType class

--- src/models/core/zombie/ZombieType.java
+++ src/models/core/zombie/ZombieType.java
@@ -7,23 +7,35 @@ public class ZombieType {
     private int damagePerTick;
     private int waveCost;
 
+    public ZombieType() {
+        this("normal zombie", 100, 0.25, 10, 1);
+    }
+
+    public ZombieType(String name, int baseHp, double speed, int damagePerTick, int waveCost) {
+        this.name = name == null || name.isBlank() ? "normal zombie" : name;
+        this.baseHp = Math.max(1, baseHp);
+        this.speed = Math.max(0, speed);
+        this.damagePerTick = Math.max(0, damagePerTick);
+        this.waveCost = Math.max(0, waveCost);
+    }
+
     public String getName() {
-        return null;
+        return name;
     }
 
     public int getBaseHp() {
-        return 0;
+        return baseHp;
     }
 
     public double getSpeed() {
-        return 0;
+        return speed;
     }
 
     public int getDamagePerTick() {
-        return 0;
+        return damagePerTick;
     }
 
     public int getWaveCost() {
-        return 0;
+        return waveCost;
     }
 }
