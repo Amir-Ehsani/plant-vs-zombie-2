b2cf7109a02dae83dcbdd6e6ad3abb9694ea76ca
amir ehsani <a.ehsani.nik@gmail.com>
2026-07-06 12:44:55 +0000

    modified Zombie class

--- src/models/core/zombie/Zombie.java
+++ src/models/core/zombie/Zombie.java
@@ -3,10 +3,18 @@ package models.core.zombie;
 import models.core.base.GameEntity;
 import models.core.projectile.Damage;
 
+import java.util.Random;
+
 public class Zombie extends GameEntity {
+    private static final Random RANDOM = new Random();
+    private static final double DROP_CHANCE = 0.10;
+    private static final String NO_DROP = "none";
+
     private double currentSpeed;
-    private boolean isGlowing;
+    private boolean glowing;
+    private boolean dropChecked;
     private boolean droppedReward;
+    private String droppedRewardType;
     private MovementStrategy movementStrategy;
     private ZombieAbility zombieAbility;
     private ZombieType type;
@@ -33,16 +41,18 @@ public class Zombie extends GameEntity {
             ZombieAbility zombieAbility
     ) {
         this.type = type == null ? new ZombieType() : type;
-        this.x = x;
-        this.y = y;
+        this.x = Math.max(0, x);
+        this.y = Math.max(0, y);
         this.maxHp = this.type.getBaseHp();
         this.hp = this.maxHp;
         this.currentSpeed = this.type.getSpeed();
         this.armor = armor;
         this.movementStrategy = movementStrategy;
         this.zombieAbility = zombieAbility;
-        this.isGlowing = false;
+        this.glowing = false;
+        this.dropChecked = false;
         this.droppedReward = false;
+        this.droppedRewardType = NO_DROP;
         this.id = buildId();
     }
 
@@ -57,14 +67,12 @@ public class Zombie extends GameEntity {
 
         if (movementStrategy != null) {
             movementStrategy.move(this);
+            normalizePosition();
             return;
         }
 
         x -= currentSpeed;
-
-        if (x < 0) {
-            x = 0;
-        }
+        normalizePosition();
     }
 
     public void attack(GameEntity target) {
@@ -72,16 +80,32 @@ public class Zombie extends GameEntity {
             return;
         }
 
-        target.takeDamage(new Damage(type.getDamagePerTick(), "bite"));
+        int damageAmount = type.getDamagePerTick();
+
+        if (damageAmount <= 0) {
+            return;
+        }
+
+        target.takeDamage(new Damage(damageAmount, "bite"));
     }
 
     public void checkDropOnDeath() {
-        if (isAlive() || droppedReward) {
+        if (isAlive() || dropChecked) {
+            return;
+        }
+
+        dropChecked = true;
+
+        if (RANDOM.nextDouble() > DROP_CHANCE) {
+            droppedReward = false;
+            droppedRewardType = NO_DROP;
+            glowing = false;
             return;
         }
 
         droppedReward = true;
-        isGlowing = true;
+        droppedRewardType = chooseDroppedRewardType();
+        glowing = true;
     }
 
     @Override
@@ -96,7 +120,9 @@ public class Zombie extends GameEntity {
             remainingDamage = armor.reduceDamage(remainingDamage);
         }
 
-        hp -= remainingDamage;
+        if (remainingDamage > 0) {
+            hp -= remainingDamage;
+        }
 
         if (hp < 0) {
             hp = 0;
@@ -145,15 +171,27 @@ public class Zombie extends GameEntity {
     }
 
     public boolean isGlowing() {
-        return isGlowing;
+        return glowing;
+    }
+
+    public boolean hasDropBeenChecked() {
+        return dropChecked;
     }
 
     public boolean hasDroppedReward() {
         return droppedReward;
     }
 
+    public String getDroppedRewardType() {
+        return droppedRewardType;
+    }
+
     public Armor getArmor() {
         return armor;
+    }
+
+    public boolean hasArmor() {
+        return armor != null && !armor.isBroken();
     }
 
     public void setMovementStrategy(MovementStrategy movementStrategy) {
@@ -176,6 +214,10 @@ public class Zombie extends GameEntity {
         currentSpeed = type.getSpeed();
     }
 
+    public void setGlowing(boolean glowing) {
+        this.glowing = glowing;
+    }
+
     public void executeAbility() {
         if (zombieAbility == null || !isAlive()) {
             return;
@@ -191,7 +233,31 @@ public class Zombie extends GameEntity {
 
         x += deltaX;
         y += deltaY;
+        normalizePosition();
+    }
 
+    public void heal(int amount) {
+        if (amount <= 0 || !isAlive()) {
+            return;
+        }
+
+        hp += amount;
+
+        if (hp > maxHp) {
+            hp = maxHp;
+        }
+    }
+
+    public void kill() {
+        if (!isAlive()) {
+            return;
+        }
+
+        hp = 0;
+        checkDropOnDeath();
+    }
+
+    private void normalizePosition() {
         if (x < 0) {
             x = 0;
         }
@@ -199,5 +265,19 @@ public class Zombie extends GameEntity {
         if (y < 0) {
             y = 0;
         }
+    }
+
+    private String chooseDroppedRewardType() {
+        int roll = RANDOM.nextInt(100);
+
+        if (roll < 70) {
+            return "coin";
+        }
+
+        if (roll < 90) {
+            return "diamond";
+        }
+
+        return "pot";
     }
 }
