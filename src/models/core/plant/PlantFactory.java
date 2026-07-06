5de4242c3e87ce84a5fd990c9bb5d3ee95a15c7d
amir ehsani <a.ehsani.nik@gmail.com>
2026-07-06 12:27:03 +0000

    modified PlantFactory class

--- src/models/core/plant/PlantFactory.java
+++ src/models/core/plant/PlantFactory.java
@@ -1,8 +1,5 @@
 package models.core.plant;
 
-import models.core.plant.behaviours.NoAttackBehavior;
-import models.core.plant.behaviours.SimpleDamageAttackBehavior;
-
 public class PlantFactory {
     private final PlantRegistry plantRegistry;
 
@@ -23,8 +20,7 @@ public class PlantFactory {
             throw new IllegalArgumentException("Plant type cannot be null.");
         }
 
-        AttackBehavior attackBehavior = createAttackBehavior(type);
-        return new Plant(type, x, y, attackBehavior);
+        return new Plant(type, x, y);
     }
 
     public Plant createPlant(String plantName, double x, double y) {
@@ -37,48 +33,7 @@ public class PlantFactory {
         return createPlant(type, x, y);
     }
 
-    private AttackBehavior createAttackBehavior(PlantType type) {
-        String category = type.getCategory();
-
-        if (isNonAttackingCategory(category)) {
-            return new NoAttackBehavior();
-        }
-
-        return createDamageBehavior(category);
-    }
-
-    private boolean isNonAttackingCategory(String category) {
-        return category.equals("sun producer")
-                || category.equals("wall-nut")
-                || category.equals("modifier")
-                || category.equals("normal");
-    }
-
-    private AttackBehavior createDamageBehavior(String category) {
-        if (category.equals("shooter")) {
-            return new SimpleDamageAttackBehavior(20, "normal");
-        }
-
-        if (category.equals("strike-through")) {
-            return new SimpleDamageAttackBehavior(20, "piercing");
-        }
-
-        if (category.equals("homing")) {
-            return new SimpleDamageAttackBehavior(30, "homing");
-        }
-
-        if (category.equals("lobber")) {
-            return new SimpleDamageAttackBehavior(40, "lobber");
-        }
-
-        if (category.equals("melee")) {
-            return new SimpleDamageAttackBehavior(15, "melee");
-        }
-
-        if (category.equals("explosive")) {
-            return new SimpleDamageAttackBehavior(1800, "explosive");
-        }
-
-        return new NoAttackBehavior();
+    public PlantRegistry getPlantRegistry() {
+        return plantRegistry;
     }
 }
