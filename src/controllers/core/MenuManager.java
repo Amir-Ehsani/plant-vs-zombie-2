dbfa9ac4acaca1ae7476f27f4d262cd6bb49fe7a
Mohammad Khosravi <m.khosravii1313@gmail.com>
2026-07-16 03:08:12 +0330

    edited MenuManager

--- src/controllers/core/MenuManager.java
+++ src/controllers/core/MenuManager.java
@@ -1,24 +1,34 @@
 package controllers.core;
 
 import controllers.auth.AuthController;
+import controllers.features.GameMenuController;
+import controllers.features.MainMenuController;
+import controllers.features.ProfileController;
+import controllers.features.SettingsController;
 import views.core.BaseView;
-import views.menus.LoginView;
-import views.menus.MainMenuView;
-import views.menus.ProfileView;
+import views.menus.*;
 
 public class MenuManager {
     private final AuthController authController;
+    private final MainMenuController mainMenuController;
+    private final ProfileController profileController;
+    private final SettingsController settingsController;
+    private final GameMenuController gameMenuController;
     private BaseView currentView;
     private String lastMessage;
 
     public MenuManager() {
         this.authController = new AuthController();
+        this.mainMenuController = new MainMenuController(authController);
+        this.profileController = new ProfileController(authController);
+        this.settingsController = new SettingsController(authController);
+        this.gameMenuController = new GameMenuController(authController);
         this.lastMessage = "";
 
         if (authController.isLoggedIn()) {
-            currentView = new MainMenuView("Main Menu", this, authController);
+            currentView = new MainMenuView("Main Menu", this, mainMenuController);
         } else {
-            currentView = new LoginView("Login Menu", this, authController);
+            currentView = new RegisterView("Register Menu", this, authController);
         }
     }
 
@@ -51,7 +61,23 @@ public class MenuManager {
         success("Current menu: " + currentView.getViewName());
     }
 
+
+    public void enterRegisterMenu() {
+        if (authController.isLoggedIn()) {
+            fail("You are already logged in. Use menu logout first.");
+            return;
+        }
+
+        changeView(new RegisterView("Register Menu", this, authController));
+        success("Entered register menu.");
+    }
+
     public void enterLoginMenu() {
+        if (authController.isLoggedIn()) {
+            fail("You are already logged in. Use menu logout first.");
+            return;
+        }
+
         changeView(new LoginView("Login Menu", this, authController));
         success("Entered login menu.");
     }
@@ -62,7 +88,7 @@ public class MenuManager {
             return;
         }
 
-        changeView(new MainMenuView("Main Menu", this, authController));
+        changeView(new MainMenuView("Main Menu", this, mainMenuController));
         success("Entered main menu.");
     }
 
@@ -72,8 +98,28 @@ public class MenuManager {
             return;
         }
 
-        changeView(new ProfileView("Profile Menu", this, authController));
+        changeView(new ProfileView("Profile Menu", this, profileController));
         success("Entered profile menu.");
+    }
+
+    public void enterSettingsMenu() {
+        if (!authController.isLoggedIn()) {
+            fail("You must login first.");
+            return;
+        }
+
+        changeView(new SettingsView("Settings Menu", this, settingsController));
+        success("Entered settings menu.");
+    }
+
+    public void enterGameMenu() {
+        if (!authController.isLoggedIn()) {
+            fail("You must login first.");
+            return;
+        }
+
+        changeView(new GameMenuView("Game Menu", this, gameMenuController));
+        success("Entered game menu.");
     }
 
     public void enterNamedMenu(String menuName) {
@@ -82,23 +128,29 @@ public class MenuManager {
             return;
         }
 
-        if ("login".equals(menuName)) {
-            enterLoginMenu();
-            return;
+        switch (menuName) {
+            case "register":
+                enterRegisterMenu();
+                break;
+            case "login":
+                enterLoginMenu();
+                break;
+            case "main":
+                enterMainMenu();
+                break;
+            case "profile":
+                enterProfileMenu();
+                break;
+            case "settings":
+                enterSettingsMenu();
+                break;
+            case "game":
+                enterGameMenu();
+                break;
+            default:
+                fail("can't find " + menuName + " menu.");
+                break;
         }
-
-        if ("main".equals(menuName)) {
-            enterMainMenu();
-            return;
-        }
-
-        if ("profile".equals(menuName)) {
-            enterProfileMenu();
-            return;
-        }
-
-
-        fail("can't find " + menuName + " menu.");
     }
 
     public void exitCurrentMenu() {
@@ -109,18 +161,18 @@ public class MenuManager {
 
         String viewName = currentView.getViewName();
 
-        if ("Login Menu".equals(viewName)) {
+        if ("Register Menu".equals(viewName)) {
             closeProgram();
+            return;
+        }
+
+        if ("Login Menu".equals(viewName)) {
+            enterRegisterMenu();
             return;
         }
 
         if ("Main Menu".equals(viewName)) {
             fail("Use menu logout to leave the main menu.");
-            return;
-        }
-
-        if ("Profile Menu".equals(viewName)) {
-            enterMainMenu();
             return;
         }
 
@@ -133,28 +185,23 @@ public class MenuManager {
         success("Program closed.");
     }
 
-    public void enterChapter(String chapterName) {
-        if (!authController.isLoggedIn()) {
-            fail("You must login first.");
-            return;
-        }
-
-        if (chapterName == null || chapterName.isBlank()) {
-            fail("Chapter name is required.");
-            return;
-        }
-
-        success("Entered chapter " + chapterName + ".");
+    public void showRegisterMenuText() {
+        success("""
+                Register Menu
+                register -u <username> -p <password> <password_confirm> -n <nickname> -e <email> -g <gender>
+                pick question -q <question_number> -a <answer> -c <answer_confirm>
+                menu enter login
+                menu show current
+                menu exit""");
     }
 
     public void showLoginMenuText() {
         success("""
-                Login/Register Menu
-                register -u <username> -p <password> <password_confirm> -n <nickname> -e <email> -g <gender>
-                pick question -q <question_number> -a <answer> -c <answer_confirm>
+                Login Menu
                 login -u <username> -p <password> -stay-logged-in
                 forget password -u <username> -e <email>
                 answer -a <answer>
+                menu enter register
                 menu show current
                 menu exit""");
     }
@@ -162,15 +209,14 @@ public class MenuManager {
     public void showMainMenuText() {
         success("""
                 Main Menu
-                menu enter <menu_name>
-                menu show current
+                menu enter game
+                menu enter settings
+                menu enter profile
+                menu enter news
+                menu enter network
                 menu logout
-                menu profile show-info
-                menu profile change-username -u <username>
-                menu profile change-nickname -u <nickname>
-                menu profile change-email -e <email>
-                menu profile change-password -p <new_password> -o <old_password>
-                menu settings change-difficulty -l <difficulty_level>""");
+                menu show current
+                menu exit""");
     }
 
     public void showProfileMenuText() {
@@ -184,6 +230,30 @@ public class MenuManager {
                 menu show current
                 menu exit""");
     }
+
+    public void showSettingsMenuText() {
+        success("""
+                Settings Menu
+                menu settings change-difficulty -l <difficulty_level>
+                menu show current
+                menu exit""");
+    }
+
+    public void showGameMenuText() {
+        success("""
+                Game Menu
+                menu enter collection
+                menu enter chapter -c <chaptername>
+                menu coin-wallet
+                menu gem-wallet
+                menu greenhouse
+                menu travel-log
+                menu leaderboard
+                menu cheat add <n> <coin/diamond>
+                menu show current
+                menu exit""");
+    }
+
 
     public void invalidCommand(String menuName) {
         fail("Invalid command in " + menuName + ".");
