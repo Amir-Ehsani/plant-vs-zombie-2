71688d90b8b835b47c9e2af66698038574ec96bb
Amir-Ehsani <137894802+Amir-Ehsani@users.noreply.github.com>
2026-08-19 22:14:43 +0430

    Reworked modal window

--- pvz-proj/core/src/main/java/ui/ModalWindow.java
+++ pvz-proj/core/src/main/java/ui/ModalWindow.java
@@ -1,23 +1,40 @@
 package ui;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import pvz.skin.BorderedTable;
 
-public class ModalWindow extends Window {
+public class ModalWindow extends Table {
+    private final BorderedTable panel;
+    private final Table content;
+
     public ModalWindow(String title, Skin skin) {
-        super(title == null ? "" : title, skin);
-        setModal(true);
-        setMovable(false);
-        pad(20f);
+        setFillParent(true);
+        addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+            }
+        });
+        panel = new BorderedTable();
+        panel.pad(28f);
+        content = new Table();
+        Label titleLabel = new Label(title == null ? "" : title, skin, "big_outline");
+        panel.add(titleLabel).growX().padBottom(16f).row();
+        panel.add(content).grow();
+        add(panel);
+    }
+
+    public Table getContentTable() {
+        return content;
     }
 
     public void show(Stage stage) {
-        pack();
-        float x = (stage.getViewport().getWorldWidth() - getWidth()) / 2f;
-        float y = (stage.getViewport().getWorldHeight() - getHeight()) / 2f;
-        setPosition(x, y);
         stage.addActor(this);
+        toFront();
     }
 
     public void close() {
