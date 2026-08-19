09d03ac6c151c204aab8bfcdf87fde669cc6a295
Amir-Ehsani <137894802+Amir-Ehsani@users.noreply.github.com>
2026-08-19 21:57:46 +0430

    Added progress bar actor

---
+++ pvz-proj/core/src/main/java/ui/ProgressBarActor.java
@@ -0,0 +1,43 @@
+package ui;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class ProgressBarActor extends Table {
+    private final ProgressBar progressBar;
+    private final Label valueLabel;
+    private float minimum;
+    private float maximum;
+
+    public ProgressBarActor(Skin skin, float minimum, float maximum) {
+        this.minimum = minimum;
+        this.maximum = Math.max(minimum + 1f, maximum);
+        progressBar = new ProgressBar(this.minimum, this.maximum, 1f, false, skin);
+        valueLabel = new Label("", skin);
+        add(progressBar).width(170f).padRight(8f);
+        add(valueLabel).width(90f);
+        setValue(this.minimum);
+    }
+
+    public void setRange(float minimum, float maximum) {
+        this.minimum = minimum;
+        this.maximum = Math.max(minimum + 1f, maximum);
+        progressBar.setRange(this.minimum, this.maximum);
+        setValue(progressBar.getValue());
+    }
+
+    public void setValue(float value) {
+        float bounded = Math.max(minimum, Math.min(maximum, value));
+        progressBar.setValue(bounded);
+        valueLabel.setText(formatValue(bounded) + " / " + formatValue(maximum));
+    }
+
+    private String formatValue(float value) {
+        if (value == Math.round(value)) {
+            return String.valueOf(Math.round(value));
+        }
+        return String.format("%.1f", value);
+    }
+}
