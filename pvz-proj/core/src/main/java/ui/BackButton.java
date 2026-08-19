b7e710ac9076189dcec4a47ea5823c146bb6db40
Amir-Ehsani <137894802+Amir-Ehsani@users.noreply.github.com>
2026-08-19 22:14:40 +0430

    Styled back button

--- pvz-proj/core/src/main/java/ui/BackButton.java
+++ pvz-proj/core/src/main/java/ui/BackButton.java
@@ -4,6 +4,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class BackButton extends MenuButton {
     public BackButton(Skin skin, Runnable action) {
-        super("Back", skin, action);
+        super("Back", skin, "brown", action);
     }
 }
