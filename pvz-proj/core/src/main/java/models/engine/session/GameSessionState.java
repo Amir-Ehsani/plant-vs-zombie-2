c2a1c46d219fb6f4766994205967ca96e962a517
Mohammad Khosravi <m.khosravii1313@gmail.com>
2026-08-30 23:12:03 +0330

    update session state

--- pvz-proj/core/src/main/java/models/engine/session/GameSessionState.java
+++ pvz-proj/core/src/main/java/models/engine/session/GameSessionState.java
@@ -68,6 +68,7 @@ abstract class GameSessionState {
     protected int plantFoodCount;
     protected int initialPlantFoodCount;
     protected int totalSunProduced;
+    protected int totalSunCollected;
     protected Level currentLevel;
     protected Board board;
     protected TickManager tickManager;
@@ -312,6 +313,10 @@ abstract class GameSessionState {
 
     public int getTotalSunProduced() {
         return totalSunProduced;
+    }
+
+    public int getTotalSunCollected() {
+        return totalSunCollected;
     }
 
     public int getTotalZombiesKilled() {
