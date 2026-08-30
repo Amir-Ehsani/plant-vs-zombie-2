8d2bb819031e26c3ad804451616ae5fa65922818
Amir-Ehsani <137894802+Amir-Ehsani@users.noreply.github.com>
2026-08-30 11:48:17 +0430

    fix(p1-08): BaseScreen

--- pvz-proj/core/src/main/java/screens/BaseScreen.java
+++ pvz-proj/core/src/main/java/screens/BaseScreen.java
@@ -8,52 +8,29 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.pvz.Main;
 import ui.NotificationManager;
-import ui.UiMotion;
 
 public abstract class BaseScreen implements Screen {
     public static final float WORLD_WIDTH = 1280f;
     public static final float WORLD_HEIGHT = 720f;
-    private static final float FADE_IN_SECONDS = 0.18f;
-    private static final float FADE_OUT_SECONDS = 0.14f;
+    private static final float FADE_IN_SECONDS = 0.14f;
 
     protected final Main game;
     protected final Stage stage;
     protected final NotificationManager notificationManager;
-    private boolean transitioning;
 
     protected BaseScreen(Main game) {
         this.game = game;
         stage = new Stage(new FitViewport(WORLD_WIDTH, WORLD_HEIGHT));
         notificationManager = new NotificationManager(stage, game.getSkin());
-        transitioning = false;
     }
 
     @Override
     public void show() {
         Gdx.input.setInputProcessor(stage);
         notificationManager.activate();
-        UiMotion.enhanceTree(stage.getRoot());
-        stage.getRoot().clearActions();
         stage.getRoot().getColor().a = 0f;
-        stage.getRoot().addAction(Actions.fadeIn(FADE_IN_SECONDS));
-        transitioning = false;
-    }
-
-    public boolean transitionOut(Runnable afterTransition) {
-        if (transitioning) {
-            return false;
-        }
-        transitioning = true;
         stage.getRoot().clearActions();
-        stage.getRoot().addAction(Actions.sequence(
-                Actions.fadeOut(FADE_OUT_SECONDS),
-                Actions.run(() -> {
-                    if (afterTransition != null) {
-                        afterTransition.run();
-                    }
-                })
-        ));
-        return true;
+        stage.getRoot().addAction(Actions.fadeIn(FADE_IN_SECONDS));
     }
 
     @Override
